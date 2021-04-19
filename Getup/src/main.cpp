fc7b5f32ba2d085e3e8bf2f8035bf1c79839d47c
Nathaniel Hudson <nchudson18@gmail.com>
2021-04-18 20:38:25 -0700

    More changes

--- Getup/src/main.cpp
+++ Getup/src/main.cpp
@@ -16,6 +16,7 @@
 #include "Adafruit_ADXL343.h"
 #include "Adafruit_LiquidCrystal.h"
 #include "RTClib.h"
+#include "Adafruit_BluefruitLE_SPI.h"
 
 //------------------------------------------------------------------------------
 //      __   ___  ___         ___  __
@@ -42,13 +43,24 @@
 #define LED_WAIT     (6)
 #define BUZZER       (4)
 
-// I2C address defines
-#define ACCEL_ADDR 
+// I2C addresses
+#define ACCEL_ADDR   (0x3A >> 1)
+#define RTC_ADDR     (0x68)
+#define LCD_ADDR     (0x20)
 
-// Program value defines
+// Device parameters
+
+#define ACCEL_ID     (0x00000000)
+
+#define LCD_WIDTH    (16)
+#define LCD_HEIGHT   (2)
+
+// Program values
 #define BUTTON_UPDATE_TIME  (20)
 #define RTC_UPDATE_TIME     (86400000)
 #define ACCEL_UPDATE_TIME   (100)
+
+//------------------------------------------------------------------------------
 //     ___      __   ___  __   ___  ___  __
 //      |  \ / |__) |__  |  \ |__  |__  /__`
 //      |   |  |    |___ |__/ |___ |    .__/
@@ -64,7 +76,7 @@ typedef enum {
   TIMER_LED,
   TIMER_FSM,
   NUM_TIMERS
-} timer_t;
+} timers_t;
 
 typedef enum {
   SETUP,
@@ -85,6 +97,8 @@ typedef enum {
 //
 //------------------------------------------------------------------------------
 
+static volatile uint64_t sysTime = 0;
+
 //------------------------------------------------------------------------------
 //      __   __   __  ___  __  ___      __   ___  __
 //     |__) |__) /  \  |  /  \  |  \ / |__) |__  /__`
@@ -100,28 +114,95 @@ typedef enum {
 //------------------------------------------------------------------------------
 
 //==============================================================================
-int main() {
+void setup() {
   // Local variables
 
   uint64_t delta = sysTime;
   uint64_t timers[NUM_TIMERS];
 
-  RTC_DS3231 rtc = new RTC_DS3231();
-  Adafruit_LiquidCrystal lcd = new Adafruit_LiquidCrystal();
-  Adafruit_ADXL343 accel = new Adafruit_ADXL343();
+  RTC_DS3231 rtc;
+  Adafruit_LiquidCrystal lcd(0);
+  Adafruit_ADXL343 accel(ACCEL_ID);
+  Adafruit_BluefruitLE_SPI ble(BT_CS, BT_IRQ);
 
 
   // System and driver initialization
+  SystemInit();
   rtc.begin();
-  lcd.begin();
-  accel.begin();
+  lcd.begin(LCD_WIDTH, LCD_HEIGHT);
+  accel.begin(ACCEL_ADDR);
+
+  //Pin configuration
+  pinMode(BTN_PLUS, INPUT);
+  pinMode(BTN_MINUS, INPUT);
+  pinMode(BTN_SEL, INPUT);
+
+  pinMode(QI_CHG, INPUT);
+  pinMode(BATT_LOW, INPUT);
+
+  pinMode(ACCEL_IRQ1, INPUT);
+  pinMode(ACCEL_IRQ2, INPUT);
+
+  pinMode(LED_WAIT, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  
 
 
+  digitalWrite(LED_WAIT, LOW);
+  lcd.setBacklight(LOW);
+  lcd.print("Hello, World!");
 
- while(true) {
-
- }
+  sysTime = millis();
+  delta = sysTime;
+  // Infinite loop of science!
 }
+void loop() {
+
+}
+// int main() {
+//   // Local variables
+// 
+//   uint64_t delta = sysTime;
+//   uint64_t timers[NUM_TIMERS];
+// 
+//   RTC_DS3231 rtc;
+//   Adafruit_LiquidCrystal lcd(0);
+//   Adafruit_ADXL343 accel(ACCEL_ID);
+//   Adafruit_BluefruitLE_SPI ble(BT_CS, BT_IRQ);
+// 
+// 
+//   // System and driver initialization
+//   SystemInit();
+//   rtc.begin();
+//   lcd.begin(LCD_WIDTH, LCD_HEIGHT);
+//   accel.begin(ACCEL_ADDR);
+// 
+//   //Pin configuration
+//   pinMode(BTN_PLUS, INPUT);
+//   pinMode(BTN_MINUS, INPUT);
+//   pinMode(BTN_SEL, INPUT);
+// 
+//   pinMode(QI_CHG, INPUT);
+//   pinMode(BATT_LOW, INPUT);
+// 
+//   pinMode(ACCEL_IRQ1, INPUT);
+//   pinMode(ACCEL_IRQ2, INPUT);
+// 
+//   pinMode(LED_WAIT, OUTPUT);
+//   pinMode(BUZZER, OUTPUT);
+//   
+// 
+// 
+//   digitalWrite(LED_WAIT, LOW);
+//   lcd.setBacklight(LOW);
+//   lcd.print("Hello, World!");
+// 
+//   sysTime = millis();
+//   delta = sysTime;
+//   // Infinite loop of science!
+//   while(1) {
+//   }
+// }
 
 //------------------------------------------------------------------------------
 //      __   __              ___  ___
@@ -140,6 +221,3 @@ int main() {
 //==============================================================================
 // Tick tock tick tock...
 //==============================================================================
-void SysTick_Handler () {
-  sysTime++;
-}
