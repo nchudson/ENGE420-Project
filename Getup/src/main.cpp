1d9393a195d3e569d41b54067bdb9448f99dbdba
Nathaniel Hudson <nchudson18@gmail.com>
2021-04-20 16:07:48 -0700

    Testing and loop stub

--- Getup/src/main.cpp
+++ Getup/src/main.cpp
@@ -41,7 +41,7 @@
 #define BT_IRQ       (7)
 
 #define LED_WAIT     (6)
-#define BUZZER       (4)
+#define BUZZER       (3)
 
 // I2C addresses
 #define ACCEL_ADDR   (0x3A >> 1)
@@ -59,6 +59,7 @@
 #define BUTTON_UPDATE_TIME  (20)
 #define RTC_UPDATE_TIME     (86400000)
 #define ACCEL_UPDATE_TIME   (100)
+#define LCD_UPDATE_TIME     (100)
 
 //------------------------------------------------------------------------------
 //     ___      __   ___  __   ___  ___  __
@@ -74,6 +75,7 @@ typedef enum {
   TIMER_BATT,
   TIMER_SPEAKER,
   TIMER_LED,
+  TIMER_LCD,
   TIMER_FSM,
   NUM_TIMERS
 } timers_t;
@@ -96,8 +98,16 @@ typedef enum {
 //      \/  /~~\ |  \ | /~~\ |__) |___ |___ .__/
 //
 //------------------------------------------------------------------------------
-
-static volatile uint64_t sysTime = 0;
+static uint32_t sys_time_temp = 0;
+static uint64_t delta = 0;
+static uint64_t timers[NUM_TIMERS];
+static volatile uint64_t sys_time = 0;
+static RTC_DS3231 rtc;
+static Adafruit_LiquidCrystal lcd(LCD_ADDR);
+static Adafruit_ADXL343 accel(ACCEL_ID);
+static Adafruit_BluefruitLE_SPI ble(BT_CS, BT_IRQ);
+static DateTime rtc_time;
+static char* lcd_time = new char[12];
 
 //------------------------------------------------------------------------------
 //      __   __   __  ___  __  ___      __   ___  __
@@ -117,13 +127,7 @@ static volatile uint64_t sysTime = 0;
 void setup() {
   // Local variables
 
-  uint64_t delta = sysTime;
-  uint64_t timers[NUM_TIMERS];
 
-  RTC_DS3231 rtc;
-  Adafruit_LiquidCrystal lcd(LCD_ADDR);
-  Adafruit_ADXL343 accel(ACCEL_ID);
-  Adafruit_BluefruitLE_SPI ble(BT_CS, BT_IRQ);
 
 
   // System and driver initialization
@@ -148,21 +152,43 @@ void setup() {
 
 
   digitalWrite(LED_WAIT, LOW);
-  tone(BUZZER, 880);
-  lcd.setBacklight(LOW);
-  lcd.print("Hello, World!");
+  // tone(BUZZER, 440);
+  lcd.setBacklight(HIGH);
 
-  sysTime = millis();
-  delta = sysTime;
+  sys_time = millis();
+  delta = sys_time;
   // Infinite loop of science!
 }
 void loop() {
-
+  // Get current millis() value and update 64-bit counter.
+  if(millis() != sys_time_temp) {
+    if(sys_time_temp < (sys_time & 0x00000000FFFFFFFF)) {
+      sys_time = ((sys_time & 0xFFFFFFFF00000000) | sys_time_temp) +
+        0x0000000100000000;
+    }
+    else {
+      sys_time = (sys_time & 0xFFFFFFFF00000000) | sys_time_temp;
+    }
+  }
+  delta = sys_time - timers[TIMER_LCD];
+  if(delta >= LCD_UPDATE_TIME) {
+    rtc_time = rtc.now();
+    lcd.setCursor(0, 0);
+    sprintf_P(lcd_time, "%.2d:%.2d:%.2d", rtc_time.hour(), rtc_time.minute(), rtc_time.second());
+    lcd.print(lcd_time);
+    lcd.setCursor(0, 1);
+    if(!digitalRead(QI_CHG)) {
+      lcd.print("    Charging    ");
+    }
+    else {
+      lcd.print("  Not Charging  ");
+    }
+  }
 }
 // int main() {
 //   // Local variables
 // 
-//   uint64_t delta = sysTime;
+//   uint64_t delta = sys_time;
 //   uint64_t timers[NUM_TIMERS];
 // 
 //   RTC_DS3231 rtc;
@@ -197,8 +223,8 @@ void loop() {
 //   lcd.setBacklight(LOW);
 //   lcd.print("Hello, World!");
 // 
-//   sysTime = millis();
-//   delta = sysTime;
+//   sys_time = millis();
+//   delta = sys_time;
 //   // Infinite loop of science!
 //   while(1) {
 //   }
