3d87c31a99140bb4b929dfdb94c1520a36cefe81
Nathaniel Hudson <nchudson18@gmail.com>
2021-04-24 16:34:54 -0700

    Sensor testing and internal RTC implementation

--- Getup/src/main.cpp
+++ Getup/src/main.cpp
@@ -16,6 +16,7 @@
 #include "Adafruit_ADXL343.h"
 #include "Adafruit_LiquidCrystal.h"
 #include "RTClib.h"
+#include "RTCZero.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 
 //------------------------------------------------------------------------------
@@ -26,25 +27,25 @@
 //------------------------------------------------------------------------------
 
 // Pin defines
-#define BTN_PLUS     (A1)
-#define BTN_MINUS    (A2)
-#define BTN_SEL      (A3)
-#define BTN_SET      (A4)
+#define PIN_PIN_BTN_PLUS     (A1)
+#define PIN_PIN_BTN_MINUS    (A2)
+#define PIN_PIN_BTN_SEL      (A3)
+#define PIN_PIN_BTN_SET      (A4)
 
-#define QI_CHG       (A0)
-#define BATT_LOW     (A5)
+#define PIN_QI_CHG       (A0)
+#define PIN_BATT_LOW     (A5)
 
-#define ACCEL_IRQ1   (10)
-#define ACCEL_IRQ2   (9)
+#define PIN_ACCEL_IRQ1   (10)
+#define PIN_ACCEL_IRQ2   (9)
 
-#define BT_CS        (8)
-#define BT_IRQ       (7)
+#define PIN_BT_CS        (8)
+#define PIN_BT_IRQ       (7)
 
-#define LED_WAIT     (6)
-#define BUZZER       (3)
+#define PIN_LED_WAIT     (6)
+#define PIN_BUZZER       (3)
 
 // I2C addresses
-#define ACCEL_ADDR   (0x3A >> 1)
+#define ACCEL_ADDR   (0x53)
 #define RTC_ADDR     (0x68)
 #define LCD_ADDR     (0x20)
 
@@ -60,6 +61,7 @@
 #define RTC_UPDATE_TIME     (86400000)
 #define ACCEL_UPDATE_TIME   (100)
 #define LCD_UPDATE_TIME     (100)
+#define FSM_UPDATE_TIME     (100)
 
 //------------------------------------------------------------------------------
 //     ___      __   ___  __   ___  ___  __
@@ -74,7 +76,7 @@ typedef enum {
   TIMER_ACCEL,
   TIMER_BATT,
   TIMER_SPEAKER,
-  TIMER_LED,
+  TIMER_PIN_LED,
   TIMER_LCD,
   TIMER_FSM,
   NUM_TIMERS
@@ -98,16 +100,20 @@ typedef enum {
 //      \/  /~~\ |  \ | /~~\ |__) |___ |___ .__/
 //
 //------------------------------------------------------------------------------
+static uint8_t accel_conf = 0;
+static uint8_t buttons[]
 static uint32_t sys_time_temp = 0;
 static uint64_t delta = 0;
 static uint64_t timers[NUM_TIMERS];
 static volatile uint64_t sys_time = 0;
-static RTC_DS3231 rtc;
+static RTC_DS3231 rtc_ext;
+static RTC rtc_int;
 static Adafruit_LiquidCrystal lcd(LCD_ADDR);
 static Adafruit_ADXL343 accel(ACCEL_ID);
 static Adafruit_BluefruitLE_SPI ble(BT_CS, BT_IRQ);
-static DateTime rtc_time;
-static char* lcd_time = new char[12];
+static DateTime rtc_ext_time;
+static char* lcd_line_0 = new char[16];
+static char* lcd_line_1 = new char[16];
 
 //------------------------------------------------------------------------------
 //      __   __   __  ___  __  ___      __   ___  __
@@ -125,40 +131,41 @@ static char* lcd_time = new char[12];
 
 //==============================================================================
 void setup() {
-  // Local variables
-
-
-
-
   // System and driver initialization
-  rtc.begin();
+  rtc_ext.begin();
+  rtc_int.begin();
   lcd.begin(LCD_WIDTH, LCD_HEIGHT);
-  accel.begin(ACCEL_ADDR);
+  accel_conf = accel.begin(ACCEL_ADDR);
 
   //Pin configuration
-  pinMode(BTN_PLUS, INPUT);
-  pinMode(BTN_MINUS, INPUT);
-  pinMode(BTN_SEL, INPUT);
+  pinMode(PIN_PIN_BTN_PLUS, INPUT);
+  pinMode(PIN_PIN_BTN_MINUS, INPUT);
+  pinMode(PIN_PIN_BTN_SEL, INPUT);
 
-  pinMode(QI_CHG, INPUT);
-  pinMode(BATT_LOW, INPUT);
+  pinMode(PIN_QI_CHG, INPUT);
+  pinMode(PIN_BATT_LOW, INPUT);
 
-  pinMode(ACCEL_IRQ1, INPUT);
-  pinMode(ACCEL_IRQ2, INPUT);
+  pinMode(PIN_ACCEL_IRQ1, INPUT);
+  pinMode(PIN_ACCEL_IRQ2, INPUT);
 
-  pinMode(LED_WAIT, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
+  pinMode(PIN_LED_WAIT, OUTPUT);
+  pinMode(PIN_BUZZER, OUTPUT);
   
-
-
-  digitalWrite(LED_WAIT, LOW);
-  // tone(BUZZER, 440);
+  // Output configuration
+  digitalWrite(PIN_LED_WAIT, LOW);
   lcd.setBacklight(HIGH);
+
+  // RTC configuration
+  rtc_int.setDate(rtc_ext.day, rtc_ext.month, rtc_ext.year);
+  rtc_int.setTime(rtc_ext.hour, rtc_ext.minute, rtc_ext.second);
 
   sys_time = millis();
   delta = sys_time;
-  // Infinite loop of science!
 }
+
+//==============================================================================
+// Infinite loop of science!
+//==============================================================================
 void loop() {
   // Get current millis() value and update 64-bit counter.
   if(millis() != sys_time_temp) {
@@ -170,65 +177,28 @@ void loop() {
       sys_time = (sys_time & 0xFFFFFFFF00000000) | sys_time_temp;
     }
   }
+
   delta = sys_time - timers[TIMER_LCD];
   if(delta >= LCD_UPDATE_TIME) {
-    rtc_time = rtc.now();
-    lcd.setCursor(0, 0);
-    sprintf_P(lcd_time, "%.2d:%.2d:%.2d", rtc_time.hour(), rtc_time.minute(), rtc_time.second());
-    lcd.print(lcd_time);
-    lcd.setCursor(0, 1);
-    if(!digitalRead(QI_CHG)) {
-      lcd.print("    Charging    ");
+    rtc_ext_time = rtc_ext.now();
+    rtc_int_time = rtc_int.time
+    sprintf_P(lcd_line_0, "    %.2d:%.2d:%.2d    ",
+      rtc_ext_time.hour(), rtc_ext_time.minute(), rtc_ext_time.second());
+    if(!digitalRead(PIN_QI_CHG)) {
+      lcd_line_1 = "    Charging    ";
     }
     else {
-      lcd.print("  Not Charging  ");
+      sprintf_P(lcd_line_1, "x:%.2dy:%.2dz:%.2d",
+        accel.getX(), accel.getY(), accel.getZ());
     }
+    lcd.setCursor(0, 0);
+    lcd.print(lcd_line_0);
+    lcd.setCursor(0, 1);
+    lcd.print(lcd_line_1);
   }
+
+  if(delta >= FSM_)
 }
-// int main() {
-//   // Local variables
-// 
-//   uint64_t delta = sys_time;
-//   uint64_t timers[NUM_TIMERS];
-// 
-//   RTC_DS3231 rtc;
-//   Adafruit_LiquidCrystal lcd(0);
-//   Adafruit_ADXL343 accel(ACCEL_ID);
-//   Adafruit_BluefruitLE_SPI ble(BT_CS, BT_IRQ);
-// 
-// 
-//   // System and driver initialization
-//   SystemInit();
-//   rtc.begin();
-//   lcd.begin(LCD_WIDTH, LCD_HEIGHT);
-//   accel.begin(ACCEL_ADDR);
-// 
-//   //Pin configuration
-//   pinMode(BTN_PLUS, INPUT);
-//   pinMode(BTN_MINUS, INPUT);
-//   pinMode(BTN_SEL, INPUT);
-// 
-//   pinMode(QI_CHG, INPUT);
-//   pinMode(BATT_LOW, INPUT);
-// 
-//   pinMode(ACCEL_IRQ1, INPUT);
-//   pinMode(ACCEL_IRQ2, INPUT);
-// 
-//   pinMode(LED_WAIT, OUTPUT);
-//   pinMode(BUZZER, OUTPUT);
-//   
-// 
-// 
-//   digitalWrite(LED_WAIT, LOW);
-//   lcd.setBacklight(LOW);
-//   lcd.print("Hello, World!");
-// 
-//   sys_time = millis();
-//   delta = sys_time;
-//   // Infinite loop of science!
-//   while(1) {
-//   }
-// }
 
 //------------------------------------------------------------------------------
 //      __   __              ___  ___
